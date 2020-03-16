08364639f1b34fc749b8a6232b7afa8a2dbeaef1
Johannes Byle <50871836+JohannesByle@users.noreply.github.com>
2020-03-16 16:30:37 -0500

    flattened review scraper output

--- driver.py
+++ driver.py
@@ -1,4 +1,3 @@
-from product_scraper import scrape
 import pickle
 from review_scraper import get_reviews
 from datetime import datetime
@@ -8,8 +7,8 @@ from importlib import import_module
 
 
 def update_website():
-
     log = open("log.txt", "w")
+
     df = pd.DataFrame()
     for name in os.listdir("product_scrapers"):
         if name.startswith("__"):
@@ -18,7 +17,7 @@ def update_website():
             print(name)
             start = datetime.now()
             log.write("[" + name + "] Start time: " + start.strftime("%m/%d/%Y %H:%M"))
-            module = import_module("product_scrapers."+name.replace(".py", ""))
+            module = import_module("product_scrapers." + name.replace(".py", ""))
             scrape_data = pd.DataFrame(module.scrape())
             df = pd.concat([df, scrape_data])
             end = datetime.now()
@@ -31,10 +30,9 @@ def update_website():
 
     pickle.dump(df, open(r"product_data.p", "wb"))
 
-    if False:
-        start = datetime.now()
-        log.write("[TR Reviews] Start time: " + start.strftime("%m/%d/%Y %H:%M"))
-        review_data = get_reviews("https://www.tobaccoreviews.com/browse")
-        pickle.dump(review_data, open(r"review_data.p", "wb"))
-        end = datetime.now()
-        log.write(", End time: " + end.strftime("%m/%d/%Y %H:%M") + ", Total time: " + str(end - start) + "\n")
+    start = datetime.now()
+    log.write("[TR Reviews] Start time: " + start.strftime("%m/%d/%Y %H:%M"))
+    review_data = pd.DataFrame(get_reviews("https://www.tobaccoreviews.com/browse"))
+    pickle.dump(review_data, open(r"review_data.p", "wb"))
+    end = datetime.now()
+    log.write(", End time: " + end.strftime("%m/%d/%Y %H:%M") + ", Total time: " + str(end - start) + "\n")
