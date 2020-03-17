ae6018dc6a99b86bcfb3d41a1e254d0f17842119
Johannes Byle <50871836+JohannesByle@users.noreply.github.com>
2020-03-16 22:39:01 -0500

    product categorizer now working

--- product_categorizer.py
+++ product_categorizer.py
@@ -2,24 +2,20 @@ from fuzzywuzzy import fuzz, process
 import pickle
 import re
 import pandas as pd
+from tqdm import tqdm
 
 
-def get_category(item, add_item=False, cat_data=False, review_data=False):
-    if not cat_data:
-        cat_data = pickle.load(open("category_list.p", "rb"))
+def get_category(item, cat_data, review_data):
     if item in cat_data["item"].values:
         tobacco = cat_data.loc[cat_data["item"] == item]
+        contains_item = True
     else:
-        if not review_data:
-            review_data = pickle.load(open("review_data.p", "rb"))
         string = simplify_string(item)
         fuzzy_blend = process.extractOne(string, review_data["full_name"].tolist(), scorer=fuzz.token_set_ratio)
         tobacco = review_data.loc[review_data["full_name"] == fuzzy_blend[0]]
-        if add_item:
-            cat_data.append({"item": item, "brand": tobacco["brand"], "blend": tobacco["blend"]})
-            pickle.dump(cat_data, open("category_list.p", "wb"))
+        contains_item = False
 
-    return {"brand": tobacco["brand"].iloc[0], "blend": tobacco["blend"].iloc[0]}
+    return {"brand": tobacco["brand"].iloc[0], "blend": tobacco["blend"].iloc[0]}, contains_item
 
 
 def simplify_string(item):
@@ -32,3 +28,22 @@ def simplify_string(item):
         string = re.sub(rule, "", string)
 
     return string.strip()
+
+
+def categorize():
+    product_data = pickle.load(open(r"product_data.p", "rb"))
+    review_data = pickle.load(open(r"review_data.p", "rb"))
+    cat_data = pickle.load(open(r"cat_data.p", "rb"))
+    product_data["brand"] = ""
+    product_data["blend"] = ""
+
+    for index, row in tqdm(product_data.iterrows(), total=product_data.shape[0]):
+        tobacco, contains_item = get_category(row["item"], cat_data, review_data)
+        product_data.at[index, "brand"] = tobacco["brand"]
+        product_data.at[index, "blend"] = tobacco["blend"]
+        if not contains_item:
+            cat_data = cat_data.append({"item": row["item"], "brand": tobacco["brand"], "blend": tobacco["blend"]},
+                                       ignore_index=True)
+
+    pickle.dump(product_data, open(r"cat_product_data.p", "wb"))
+    pickle.dump(cat_data, open(r"cat_data.p", "wb"))
