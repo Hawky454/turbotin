0aaaf376f910d7eb04c7fd9dbee1eeaadf83bd09
Johannes Byle <50871836+JohannesByle@users.noreply.github.com>
2020-05-02 22:57:38 -0400

    refactored generate_html to make most of the processing happen outside the loop using pandas

--- html_generator/__init__.py
+++ html_generator/__init__.py
@@ -6,6 +6,7 @@ import os
 from html_generator.generate_plot import generate_plot
 from html_generator.generate_table import generate_table
 from html_generator.generate_index import generate_index
+import numpy as np
 
 
 def generate_html(df, plot_data, path):
@@ -35,37 +36,41 @@ def generate_html(df, plot_data, path):
                     ["<!--PRICE-->", "price"],
                     ["<!--STORE-->", "store"]]
 
-    data = df.groupby(['brand', 'blend'])
     files = []
     template_string = open(os.path.join(path, "templates/main_template.html"), "r").read()
     template_string = minify(template_string.replace("<!--LIST-->", index_string))
-    for blend in tqdm(data, desc="Generating html"):
-        url = slugify(blend[0][0] + " " + blend[0][1])
-        string = template_string
-        string = string.replace("<!--BLEND NAME-->", blend[0][1])
-        string = string.replace("<!--TITLE-->", "Turbotin - " + blend[0][1])
-        list_string = ""
-        item_data = blend[1].reset_index(drop=True).T.to_dict().values()
-        for row in item_data:
-            try:
-                row["real-price"] = float(row["price"][1:])
-            except:
-                row["real-price"] = float(9 * 10 ** 9)
 
-        for row in sorted(item_data, key=lambda i: i['real-price']):
-            temp_string = item_card
-            if row["stock"] == "Out of stock":
-                temp_string = temp_string.replace("<!--TIME-->", '''<div class="stock">Out of stock</div>''')
-            for n in replace_list:
-                temp_string = temp_string.replace(n[0], row[n[1]])
-            list_string = list_string + "\n" + temp_string
-        string = string.replace("<!--ITEM LIST-->", list_string)
-        string = string.replace("<!--PLOT-->", generate_plot(plot_data, blend[0][0], blend[0][1]))
+    # Convert prices to numbers and make errors very large so that they are listed last
+    df["num-price"] = pd.to_numeric(df["price"].str[1:], errors="coerce").fillna(10 ** 9)
+
+    # Convert time to out of stock and color red if out of stock
+    df["time"] = np.where(df["stock"] != "Out of stock", df["time"], '''<div class="stock">Out of stock</div>''')
+
+    # Replace all values in replace list with correct data for every row in dataframe
+    post_string = item_card
+    df["item-card"] = ""
+    for pair in replace_list:
+        value, key = pair
+        pre_string, post_string = post_string.split(value)
+        df["item-card"] = df["item-card"] + pre_string + df[key]
+    df["item-card"] = df["item-card"] + post_string
+
+    # Create the actual html page from the template
+    for index, data in tqdm(df.groupby(['brand', 'blend']), desc="Generating html"):
+        brand, blend = index
+        url = slugify(brand + " " + blend)
+        string = template_string
+        string = string.replace("<!--BLEND NAME-->", blend)
+        string = string.replace("<!--TITLE-->", "Turbotin - " + blend)
+        string = string.replace("<!--ITEM LIST-->", data["item-card"].str.cat(sep="\n"))
+        string = string.replace("<!--PLOT-->", generate_plot(plot_data, brand, blend))
         open(os.path.join(save_path, url + ".html"), "w").write(string)
         files.append(url + ".html")
 
     files.append("full_table.html")
     generate_table(df, path, save_path)
+
+    # Delete files that weren't updated (This is to prevent the directory from getting clogged with old files)
     for file in os.listdir(save_path):
         if file not in files:
             os.remove(os.path.join(save_path, file))
--- html_generator/generate_plot.py
+++ html_generator/generate_plot.py
@@ -10,13 +10,19 @@ def generate_plot(data, brand, blend):
 
     # Get list of stores that have sold that blend
     string = ["data.addColumn('date', 'Date');"]
-    col_string = "data.addColumn('number', '<!--STORE-->');\n\tdata.addColumn({type:'boolean',role:'certainty'});"
     stores = df.store.unique()
+
+    # String that tells google charts to add a new column
+    col_string = "data.addColumn('number', '<!--STORE-->');\n\tdata.addColumn({type:'boolean',role:'certainty'});"
     for store in stores:
         string.append(col_string.replace("<!--STORE-->", store))
+
+    # Sort df so that chart is in correct order, and delete useless data
     df = df.sort_values("datetime")
     df = df[["date", "store", "charts-var"]]
     df = df.reset_index()
+
+    # Reshape the data so that the row indices as the dates, the columns are the stores and the values are the
     df = df.groupby(["date", "store"])["charts-var"].aggregate("min").unstack()
     df = df.fillna("null,false")
     df["string"] = df[df.columns[0]].str.cat(df[df.columns[1:]], sep=",")
