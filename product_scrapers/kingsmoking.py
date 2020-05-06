c92b853645ed112978d3bfb2dd9449f6b57e19c3
Johannes Byle <50871836+JohannesByle@users.noreply.github.com>
2020-05-06 17:19:42 -0400

    fixed scraper, failed due to change in class name

--- product_scrapers/kingsmoking.py
+++ product_scrapers/kingsmoking.py
@@ -18,34 +18,32 @@ def scrape(pbar=None):
             allinks.add(link)
     for link in allinks:
         error = True
-        wait_time = 2.
+        wait_time = 2.75
         while error:
             try:
                 new_soup = get_html(link)
-                wait_time = 2.75
                 error = False
             except:
                 time.sleep(wait_time)
                 print("An Error Occurred: sleeping " + str(wait_time) + "s")
                 wait_time = wait_time + 1
-            pass
-            for product in new_soup.find_all('ul', class_="_2Irj0"):
-                for element in product.find_all('li'):
-                    if element.find("h3", class_="_2BULo"):
-                        item = element.find("h3", class_="_2BULo").get_text().strip()
-                    if element.find("a", class_="_2zTHN"):
-                        link = element.find("a", class_="_2zTHN").get("href")
-                    if element.find("span", class_="_23ArP"):
-                        price = element.find("span", class_="_23ArP").get_text().strip()
-                    if element.find("span", class_="_3DJ-f"):
-                        stock = element.find("span", class_="_3DJ-f").get_text().strip()
-                    if stock != "Out of stock":
-                        stock = "In Stock"
+        for product in new_soup.find_all('ul', class_="_3g8J4 _3Xnzg"):
+            for element in product.find_all('li'):
+                if element.find("h3", class_="_2BULo"):
+                    item = element.find("h3", class_="_2BULo").get_text().strip()
+                if element.find("a"):
+                    link = element.find("a").get("href")
+                if element.find("span", class_="_23ArP"):
+                    price = element.find("span", class_="_23ArP").get_text().strip()
+                if element.find("span", class_="_3DJ-f"):
+                    stock = element.find("span", class_="_3DJ-f").get_text().strip()
+                if stock != "Out of stock":
+                    stock = "In Stock"
 
-                    data.append({"store": name, "item": item, "price": price, "stock": stock, "link": link,
-                                 "time": datetime.now().strftime("%m/%d/%Y %H:%M")})
-                    if pbar is not None:
-                        pbar.set_description(", ".join([name, item]))
-                    item, price, stock, link = ["", "", "", ""]
+                data.append({"store": name, "item": item, "price": price, "stock": stock, "link": link,
+                             "time": datetime.now().strftime("%m/%d/%Y %H:%M")})
+                if pbar is not None:
+                    pbar.set_description(", ".join([name, item]))
+                item, price, stock, link = ["", "", "", ""]
 
     return data
