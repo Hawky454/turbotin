2549538ce276f6af3dd8ad4c96a09bd6c5813261
Johannes Byle <50871836+JohannesByle@users.noreply.github.com>
2020-05-06 16:15:02 -0400

    search bars now have placeholders

--- html_generator/generate_table.py
+++ html_generator/generate_table.py
@@ -20,8 +20,11 @@ def generate_table(df, path, save_path):
 
     # Specify rows that are out of stock
     soup = BeautifulSoup(string, features="lxml")
-    for i in soup.find_all("tr"):
-        if "Out of stock" in str(i):
-            i["class"] = "out-of-stock"
+    rows = soup.find_all("tr")
+    for th in rows[0].find_all("th"):
+        th["data-placeholder"] = "Search..."
+    for row in rows:
+        if "Out of stock" in str(row):
+            row["class"] = "out-of-stock"
     with open(os.path.join(save_path, "full_table.html"), "w", encoding="utf-8") as f:
         f.write(minify(str(soup)))
