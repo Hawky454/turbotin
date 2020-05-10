6cf4cc06a49644747f2c1e162abc38204dbe15bd
Johannes Byle <50871836+JohannesByle@users.noreply.github.com>
2020-05-09 22:20:26 -0400

    added scraper payless

---
+++ product_scrapers/payless.py
@@ -0,0 +1,36 @@
+from product_scrapers import get_html
+from datetime import datetime
+import re
+
+
+def scrape(pbar=None):
+    item, price, stock, link = ["", "", "", ""]
+    data = []
+    name = "payless"
+    url = "https://paylesscigarsandpipes.com/tinned-pipe-tobacco?pagesize=48&viewMode=grid&orderBy=5&pageNumber="
+
+    next_page = True
+    page_number = 1
+    while next_page:
+        soup = get_html(url + str(page_number))
+        for product in soup.find_all("div", class_="item-box"):
+            link = "https://paylesscigarsandpipes.com" + product.find("a").get("href")
+            item = product.find("h2", class_="product-title").get_text()
+            item = " ".join(item.split())
+            price = product.find("span", class_="actual-price").get_text()
+            if product.find_all("div", class_="out-of-stock"):
+                stock = "Out of stock"
+            else:
+                stock = "In stock"
+
+            data.append({"store": name, "item": item, "price": price, "stock": stock, "link": link,
+                         "time": datetime.now().strftime("%m/%d/%Y %H:%M")})
+            if pbar is not None:
+                pbar.set_description(", ".join([name, item]))
+            item, price, stock, link = ["", "", "", ""]
+        if soup.find_all("li", class_="next-page"):
+            page_number += 1
+        else:
+            next_page = False
+
+    return data
