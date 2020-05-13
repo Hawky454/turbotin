1f60ac1d688eefd1f372835cc1fef9806a964529
Johannes Byle <50871836+JohannesByle@users.noreply.github.com>
2020-05-12 20:23:41 -0500

    converted boswell to using chromedriver

--- product_scrapers/boswell.py
+++ product_scrapers/boswell.py
@@ -1,6 +1,7 @@
 from datetime import datetime
-from pyvirtualdisplay import Display
+import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 
 
@@ -10,40 +11,49 @@ def scrape(pbar=None):
     name = "boswell"
     boswell_url = ["https://boswellpipes.com/product-category/boswell-tobacco/",
                    "https://boswellpipes.com/product-category/tobacco-blends/"]
+    chrome_options = webdriver.ChromeOptions()
+    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
 
     for b_url in boswell_url:
 
-        display = Display(visible=0, size=(800, 600))
-        display.start()
-
         try:
-            # we can now start Firefox and it will run inside the virtual display
-            browser = webdriver.Firefox()
+            browser = webdriver.Chrome(options=chrome_options)
             browser.get(b_url)
             browser.find_element_by_xpath(r'''//*[@id="age-verification"]/div/button[1]''').click()
-            element = browser.find_element_by_id(r'''sb-infinite-scroll-load-more''')
-            while element.text == "Load More Products":
-                element.click()
-                style = element.get_attribute("style")
-                while style == "display: none;":
-                    style = browser.find_element_by_id(r'''sb-infinite-scroll-load-more''').get_attribute(
-                        "style")
-                element = browser.find_element_by_id(r'''sb-infinite-scroll-load-more''')
-            soup = BeautifulSoup(browser.page_source.encode('utf8'), "lxml")
+            links = []
+            for element in browser.find_elements_by_class_name("product-category"):
+                links.append(element.find_element_by_xpath(".//a").get_attribute("href"))
+            for link in links:
+                browser.get(link)
+                html = browser.find_element_by_tag_name('html')
+                loading = True
+                scroll_max = 20
+                scroll_count = 0
+                while loading and scroll_count < scroll_max:
+                    scroll_count += 1
+                    html.send_keys(Keys.END)
+                    time.sleep(1)
+                    element_id = "sb-infinite-scroll-load-more"
+                    if not browser.find_elements_by_id(element_id):
+                        break
+                    if browser.find_element_by_id(element_id).text == "No more products available...":
+                        loading = False
+
+                soup = BeautifulSoup(browser.page_source.encode('utf8'), "lxml")
+
+                for product in soup.find_all("li", class_="type-product"):
+                    item = product.find("h2", class_="woocommerce-loop-product__title").get_text()
+                    price = product.find(class_="woocommerce-Price-amount amount").get_text()
+                    link = product.find("a", class_="woocommerce-LoopProduct-link").get("href")
+                    stock = "In Stock"
+                    data.append({"store": "boswell", "item": item, "price": price, "stock": stock, "link": link,
+                                 "time": datetime.now().strftime("%m/%d/%Y %H:%M")})
+                    if pbar is not None:
+                        pbar.set_description(", ".join([name, item]))
+                    item, price, stock, link = ["", "", "", ""]
         finally:
             # tidy-up
             browser.quit()
-            display.stop()  # ignore any output from this.
-
-        for product in soup.find_all("li", class_="type-product"):
-            item = product.find("h2", class_="woocommerce-loop-product__title").get_text()
-            price = product.find(class_="woocommerce-Price-amount amount").get_text()
-            link = product.find("a", class_="woocommerce-LoopProduct-link").get("href")
-            stock = "In Stock"
-            data.append({"store": "boswell", "item": item, "price": price, "stock": stock, "link": link,
-                         "time": datetime.now().strftime("%m/%d/%Y %H:%M")})
-            if pbar is not None:
-                pbar.set_description(", ".join([name, item]))
-            item, price, stock, link = ["", "", "", ""]
 
     return data
