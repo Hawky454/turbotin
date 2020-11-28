e9d374c2d9be5d1f5aac1a162bcd461540fcd3fb
johannesbyle <johannesbyle1@gmail.com>
2020-11-28 13:04:39 +0100

    moved full_table into own directory

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,52 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import func
 import json
 import os
-import pandas as pd
-from datetime import timedelta
-
-path = os.path.dirname(__file__)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["DATABASE_URL"]
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
 db.init_app(app)
+from .views.full_table import full_table_blueprint
 
-from .models import Tobacco
+path = os.path.dirname(__file__)
 
-max_time = db.session.query(func.max(Tobacco.time)).scalar() - timedelta(hours=10)
-main_df = pd.read_sql(Tobacco.query.filter(Tobacco.time >= max_time).statement, db.session.bind)
+app.register_blueprint(full_table_blueprint)
 
 
 @app.route('/')
 def main():
-    global main_df
-    df = main_df.copy()
-
-    df["price_num"] = df["price"].str.extract(r'(\d+.\d+)')
-    df["price_num"] = pd.to_numeric(df["price_num"], errors="coerce").fillna(10 ** 4)
-    df = df[df["item"] != ""]
-
-    df["item_class"] = "text-dark"
-    df.loc[df["stock"] == "Out of stock", "item_class"] = "text-danger"
-    df["item"] = "<a class='" + df["item_class"] + "' target='_blank' href='" + df["link"] + "'>" + df["item"] + "</a>"
-    df["stock"] = "<div class='" + df["item_class"] + "'>" + df["stock"] + "</div>"
-
-    cols = ["store", "item", "stock", "price", "time", "price_num"]
-    df = df[cols]
-    
-    df = df.rename(columns={"time": "last updated (utc)"})
-    df = df.rename(columns={"item": render_template("table_forms/search_form.html", label="item", column=1)})
-    df = df.rename(columns={"store": render_template("table_forms/search_form.html", label="store", column=0)})
-    df = df.rename(columns={"stock": render_template("table_forms/stock_toggle_button.html")})
-    df = df.rename(columns={"price": render_template("table_forms/price_sort_button.html")})
-
-    table = df.to_html(index=False, classes="table table-hover display table-bordered table-responsive-lg",
-                       escape=False, border=0, justify="left", table_id="myTable")
-    return render_template("main.html", table=table)
+    return redirect("/full_table", code=302)
 
 
 @app.route('/faq')
---
+++ flask_app/views/full_table.py
@@ -0,0 +1,36 @@
+from .. import app, db
+from flask import render_template, Blueprint
+from ..models import Tobacco
+import pandas as pd
+from sqlalchemy import func
+from datetime import timedelta
+
+full_table_blueprint = Blueprint('full_table', __name__, template_folder='templates')
+
+max_time = db.session.query(func.max(Tobacco.time)).scalar() - timedelta(hours=10)
+df = pd.read_sql(Tobacco.query.filter(Tobacco.time >= max_time).statement, db.session.bind)
+
+df["price_num"] = df["price"].str.extract(r'(\d+.\d+)')
+df["price_num"] = pd.to_numeric(df["price_num"], errors="coerce").fillna(10 ** 4)
+df = df[df["item"] != ""]
+
+df["item_class"] = "text-dark"
+df.loc[df["stock"] == "Out of stock", "item_class"] = "text-danger"
+df["item"] = "<a class='" + df["item_class"] + "' target='_blank' href='" + df["link"] + "'>" + df["item"] + "</a>"
+df["stock"] = "<div class='" + df["item_class"] + "'>" + df["stock"] + "</div>"
+
+cols = ["store", "item", "stock", "price", "time", "price_num"]
+df = df[cols]
+
+
+@full_table_blueprint.route('/full_table')
+def main():
+    global df
+    df = df.rename(columns={"time": "last updated (utc)"})
+    df = df.rename(columns={"item": render_template("table_forms/search_form.html", label="item", column=1)})
+    df = df.rename(columns={"store": render_template("table_forms/search_form.html", label="store", column=0)})
+    df = df.rename(columns={"stock": render_template("table_forms/stock_toggle_button.html")})
+    df = df.rename(columns={"price": render_template("table_forms/price_sort_button.html")})
+    table = df.to_html(index=False, classes="table table-hover display table-bordered table-responsive-lg",
+                       escape=False, border=0, justify="left", table_id="myTable")
+    return render_template("main.html", table=table)
