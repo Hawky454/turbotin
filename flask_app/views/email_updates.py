717fef03d67ae2b499386847abf42b0fc3895e90
johannesbyle <johannesbyle1@gmail.com>
2020-12-01 22:01:13 +0100

    made alert sound less scary

--- flask_app/views/email_updates.py
+++ flask_app/views/email_updates.py
@@ -1,14 +1,19 @@
 from .. import db, path
 from flask import render_template, Blueprint
+from .full_table import main_df
 from ..models import Tobacco
 import pandas as pd
-from sqlalchemy import func
-from datetime import timedelta
 import os
 
 email_updates_blueprint = Blueprint('email_updates', __name__, template_folder='templates')
 
+df = main_df.copy()
+brands = list(pd.unique(df["brand"]))
+blends = {}
+for brand, data in df.groupby("brand"):
+    blends[brand] = list(pd.unique(data["blend"]))
+
 
 @email_updates_blueprint.route('/email_updates')
 def main():
-    return render_template("email_updates.html")
+    return render_template("email_updates.html", brands=brands, blends=blends)
