c08d9c5c56e85e496f38d76f8e89f627c8a73617
johannesbyle <johannesbyle1@gmail.com>
2020-12-26 22:51:47 +0100

    search bar working with proper show hide

---
+++ flask_app/templates/elements/search_bar.js
@@ -0,0 +1,25 @@
+function update_search(id, items, dropdown_element) {
+    var results_element = document.getElementById(id + '_results');
+    var input_element = document.getElementById(id + "_input");
+
+    var query = input_element.value.toLowerCase();
+
+    var new_inner_html = "";
+
+    for (var i = 0; i < items.length; i++) {
+        var index = items[i].toLowerCase().indexOf(query);
+        if (index > -1) {
+            var txt = items[i]
+            txt = txt.slice(0, index) + "<b>" + txt.slice(index, index + query.length) + "</b>" + txt.slice(index + query.length);
+            new_inner_html += `<li><a class="dropdown-item" href="#">${txt}</a></li>`;
+        }
+    }
+    results_element.innerHTML = new_inner_html;
+    if (new_inner_html === "") {
+        dropdown_element.hide();
+    } else {
+        dropdown_element.show();
+    }
+
+    return true;
+}
