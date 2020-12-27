bafd0dd9022bddbcb2876f68bc3d89a20ddff4a0
johannesbyle <johannesbyle1@gmail.com>
2020-12-27 18:08:49 +0100

    individuals blends now working as expected

--- flask_app/templates/elements/search_bar.js
+++ flask_app/templates/elements/search_bar.js
@@ -2,18 +2,25 @@ function update_search(id, items, dropdown_element) {
     var results_element = document.getElementById(id + '_results');
     var input_element = document.getElementById(id + "_input");
     var query = input_element.value.toLowerCase();
-    var new_inner_html = "";
-
+    results_element.innerHTML = "";
+    var results = 0;
+    var max_results = 50;
     for (var i = 0; i < items.length; i++) {
-        var index = items[i].toLowerCase().indexOf(query);
-        if (index > -1) {
-            var txt = items[i]
-            txt = txt.slice(0, index) + "<b>" + txt.slice(index, index + query.length) + "</b>" + txt.slice(index + query.length);
-            new_inner_html += `<li><a class="dropdown-item" href="#">${txt}</a></li>`;
+        if (results > max_results) {
+            results_element.innerHTML += `<li class='dropdown-item text-secondary'> Only showing first ${max_results} results</li>`
+            break;
         }
+        var index = items[i].text.toLowerCase().indexOf(query);
+        if (index > -1) {
+            var txt = items[i].text
+            txt = txt.slice(0, index) + "<b>" + txt.slice(index, index + query.length) + "</b>" + txt.slice(index + query.length);
+            results_element.innerHTML += `<li><a class="dropdown-item" href="${items[i].link}">${txt}</a></li>`;
+            results++;
+        }
+
     }
-    results_element.innerHTML = new_inner_html;
-    if (new_inner_html === "") {
+    if (results_element.innerHTML === "") {
+        results_element.innerHTML = "<small class='dropdown-item disabled'>No results found</small>"
         if (input_element.classList.contains("show"))
             dropdown_element.hide();
     } else {
