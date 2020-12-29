8ddf29770efd38cae1c4b3cc28b154a24568ead2
johannesbyle <johannesbyle1@gmail.com>
2020-12-29 21:12:08 +0100

    changed size to hopefully allow it to run on pyhtonanywhere

--- flask_app/models.py
+++ flask_app/models.py
@@ -4,10 +4,10 @@ from . import db
 
 class Tobacco(db.Model):
     store = db.Column(db.String(100))
-    item = db.Column(db.String(200), primary_key=True)
+    item = db.Column(db.String(150), primary_key=True)
     price = db.Column(db.String(100))
     stock = db.Column(db.String(100))
-    link = db.Column(db.String(300), primary_key=True)
+    link = db.Column(db.String(250), primary_key=True)
     time = db.Column(db.DateTime(), primary_key=True)
     brand = db.Column(db.String(500))
     blend = db.Column(db.String(500))
